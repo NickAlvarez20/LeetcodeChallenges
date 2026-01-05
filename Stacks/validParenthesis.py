792af621855fcda8f4e9953ed33222e4623ddf06
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-01-05 03:55:30 -0700

    update: Update file name and start revisit validParenthesis

--- Stacks/validParenthesis.py
+++ Stacks/validParenthesis.py
@@ -3,4 +3,4 @@ class Solution:
         stack = []
 
 
-        # Intuition: So for valid parentheses we can use a stack and compare the item on the top of the stack that we had just appended as well as any other parentheses that's contained
+        # Intuition: We can use a stack
