a670b0df1504d7683b4ff1a9e8d4b20ae7fbf090
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-03-25 15:36:23 -0700

    learn: learn and solve pow using recursion and halving

---
+++ DSA/exercise-1.py
@@ -0,0 +1,7 @@
+my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}
+
+my_dict['profession'] = 'Doctor'
+my_dict['age'] = 40
+
+print(my_dict)
+print(my_dict['city'])
---
+++ DSA/exercise-2.py
@@ -0,0 +1,23 @@
+ch_dict = {
+    # Core Identity
+    "name": "Aeliana Sunstrider",
+    "race": "High Elf",
+    "class": "Solar Cleric",
+    "level": 12,
+    # Primary Attributes (Classic RPG Stats)
+    "strength": 10,
+    "dexterity": 14,
+    "constitution": 12,
+    "intelligence": 16,
+    "wisdom": 20,
+    "charisma": 15,
+    # Combat & Status
+    "hit_points": 84,
+    "armor_class": 18,
+    "alignment": "Lawful Good",
+    "languages": ["Common", "Elvish", "Celestial"],
+    "inventory": ["Sunspire Staff", "Holy Symbol", "Healing Potion"],
+}
+
+
+print(ch_dict)
