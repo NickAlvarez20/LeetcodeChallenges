b561b0210afe831e56743eb033167b919adc19af
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-03-25 19:22:47 -0700

    solve: solve and learn dictionary and prefix solution for this problem

--- HashTables/how-many-numbers-are-smaller-than-current.py
+++ HashTables/how-many-numbers-are-smaller-than-current.py
@@ -8,3 +8,25 @@ class Solution:
                     curr_count += 1
             counts.append(curr_count)
         return counts
+
+
+# dictionary and prefix sum approach : butter algorithms
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        counts = {}
+
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+
+        # sort the unique nums
+        sorted_keys = sorted(counts.keys())
+
+        smaller_counts_map = {}
+        running_sum = 0
+
+        for key in sorted_keys:
+            smaller_counts_map[key] = running_sum
+            running_sum += counts[key]
+
+        return [smaller_counts_map[num] for num in nums]
