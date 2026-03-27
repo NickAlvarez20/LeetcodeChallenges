fa129fd7499e8b5884d0785f58f0b4759670cfc6
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-03-26 20:13:34 -0700

    update: Update with second variaiton learning

--- HashTables/max-num-of-strring-pairs.py
+++ HashTables/max-num-of-strring-pairs.py
@@ -11,3 +11,15 @@ class Solution:
             else:
                 seen.add(word)
         return count
+
+
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        count = 0
+        seen = set()
+        for word in words:
+            if "".join(reversed(word)) in seen:
+                count += 1
+            else:
+                seen.add(word)
+        return count
