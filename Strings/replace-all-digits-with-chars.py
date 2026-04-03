bd8b1e1bb3c4aa94acc2d628090a898bfc3ea0ee
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-04-03 05:18:34 -0700

    solve: Solve and practice replace-all-digit-with-char

--- Strings/replace-all-digits-with-chars.py
+++ Strings/replace-all-digits-with-chars.py
@@ -1,14 +1,28 @@
 class Solution:
     def replaceDigits(self, s: str) -> str:
+        # create helper function
         def shift(char, digit):
-            return chr(ord(char)+int(digit)) # have to convert to ord num, convert digit to int and add     
+            return chr(ord(char) + int(digit))
+        
         res = ""
+
         for index, val in enumerate(s):
             if val.isdigit():
                 res += shift(s[index-1], val)
             else:
-                res += s[index]
+                res += val
         return res
-
-
+    
+# alternative variation using range:
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        def shift(char, digit):
+            return chr(ord(char)+int(digit))
+        res = ""
+        for i in range(len(s)):
+            if s[i].isdigit():
+                res += shift(s[i-1], s[i])
+            else:
+                res += s[i]
+        return res
         
