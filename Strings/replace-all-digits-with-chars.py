72782c7bc3b748116b7ba36712dce63358a1e3db
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-04-03 03:22:16 -0700

    solve: Solve and learn replace digits with enum variation

--- Strings/replace-all-digits-with-chars.py
+++ Strings/replace-all-digits-with-chars.py
@@ -1,15 +1,14 @@
-import string
 class Solution:
     def replaceDigits(self, s: str) -> str:
         def shift(char, digit):
-            return chr(ord(char) + int(digit))
-        
+            return chr(ord(char)+int(digit)) # have to convert to ord num, convert digit to int and add     
         res = ""
-
-        for i in range(len(s)):
-            if s[i].isdigit():
-                res += shift(s[i-1], s[i])
+        for index, val in enumerate(s):
+            if val.isdigit():
+                res += shift(s[index-1], val)
             else:
-                res += s[i]
+                res += s[index]
         return res
+
+
         
