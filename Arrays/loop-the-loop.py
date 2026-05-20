1fc7c70667b5080802134bc6920d9720cfc0074b
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-05-20 15:49:09 -0700

    practice: Practice while loops

---
+++ Arrays/loop-the-loop.py
@@ -0,0 +1,8 @@
+# List of fruits to include in a fruit salad
+fruits = ["apple", "banana", "orange", "kiwi", "melon"]
+
+index = 0
+
+while index < len(fruits):
+    print(fruits[index])
+    index += 1
