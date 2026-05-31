cca4a93a9b806bb273c91ae7a92458f07c54bf7f
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-05-31 16:43:42 -0700

    solves: Solves the fun maths problems

--- Arrays/cosmo.py
+++ Arrays/cosmo.py
@@ -1,17 +1,16 @@
-def solution(n):
-    num_odds = 0
-    digit_product = 1
-    while n > 0:
-        digit = n % 10
-        # if odd, increment num_odds and multiply
-        if digit % 2 == 1:
-            num_odds += 1
-            digit_product *= digit
-        elif digit % 2 == 0:
-            pass
-        n = n // 10
-    if num_odds == 0:
-        return 0
-    else:
-        return digit_product
+import math
 
+
+def solution(n):
+    result = 0
+    curr_class = int(math.log10(n) + 1)
+    # get rid of trailing 0's
+    while n > 0:
+        digit = n % 10  # grabs last digit
+        curr_class = int(math.log10(n))
+        if digit == 0:
+            pass
+        else:
+            result += digit * math.pow(10, curr_class)
+        n = n // 10  # removes last digit
+    return int(result)
--- Arrays/cosmo_test.py
+++ Arrays/cosmo_test.py
@@ -4,34 +4,34 @@ from cosmo import solution
 
 class SolutionTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(solution(43172), 21)
+        self.assertEqual(solution(12345), 54321)
 
     def test2(self):
-        self.assertEqual(solution(2468), 0)
+        self.assertEqual(solution(1), 1)
 
     def test3(self):
-        self.assertEqual(solution(11111), 1)
+        self.assertEqual(solution(87654321), 12345678)
 
     def test4(self):
-        self.assertEqual(solution(10), 1)
+        self.assertEqual(solution(54321), 12345)
 
     def test5(self):
-        self.assertEqual(solution(39991), 2187)
+        self.assertEqual(solution(87654321), 12345678)
 
     def test6(self):
-        self.assertEqual(solution(73004), 21)
+        self.assertEqual(solution(56), 65)
 
     def test7(self):
-        self.assertEqual(solution(123456), 15)
+        self.assertEqual(solution(4567890), 987654)
 
     def test8(self):
-        self.assertEqual(solution(77777), 16807)
+        self.assertEqual(solution(98765), 56789)
 
     def test9(self):
-        self.assertEqual(solution(33333333), 6561)
+        self.assertEqual(solution(876), 678)
 
     def test10(self):
-        self.assertEqual(solution(100000000), 1)
+        self.assertEqual(solution(23456789), 98765432)
 
 
 if __name__ == "__main__":
