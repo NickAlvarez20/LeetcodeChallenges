36157c05c15cadb13ea38cbfa01a42c765167f79
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-05-31 14:58:40 -0700

    solve: Solve reverse number in list with tuples

--- Arrays/cosmo.py
+++ Arrays/cosmo.py
@@ -1,10 +1,27 @@
-import math
-
-
 def solution(numbers):
-    result = []
     n = len(numbers)
+    result = []
+
     for i in range(n):
-        square_root = round(math.sqrt((numbers[i] * numbers[n - i - 1])), 2)
-        result.append((numbers[i], square_root))
+        # reverse the current elem, and store for later use
+        reverse_elem = str(numbers[i])[::-1]
+        int_reverse = int(reverse_elem)
+        original_num = str(int_reverse)[::-1]
+        original_num_int = int(original_num)
+
+        # iterate through check if current elem has a reverse_elem, if not skip with pass
+        if numbers[i] == original_num_int and int_reverse in numbers:
+            result.append((numbers[i], int(reverse_elem)))
+        elif int_reverse == original_num_int and int_reverse in numbers:
+            result.append((numbers[i], int(reverse_elem)))
+        else:
+            pass
     return result
+
+
+nums = [10, 1, 20, 2]
+
+
+test_case = solution(nums)
+
+print(test_case)
--- Arrays/cosmo_test.py
+++ Arrays/cosmo_test.py
@@ -1,64 +1,37 @@
 import unittest
-from math import sqrt
-
 from cosmo import solution
 
 
 class SolutionTests(unittest.TestCase):
     def test1(self):
-        self.assertEqual(
-            solution([1, 2, 3, 4, 5]),
-            [(1, 2.24), (2, 2.83), (3, 3.0), (4, 2.83), (5, 2.24)],
+        self.assertListEqual(
+            solution([12, 21, 34, 43, 56, 65]),
+            [(12, 21), (21, 12), (34, 43), (43, 34), (56, 65), (65, 56)],
         )
 
     def test2(self):
-        self.assertEqual(
-            solution([3, 2, 1, 0, 1, 2, 3]),
-            [(3, 3.0), (2, 2.0), (1, 1.0), (0, 0.0), (1, 1.0), (2, 2.0), (3, 3.0)],
+        self.assertListEqual(
+            solution([10, 1, 20, 2]), [(10, 1), (1, 1), (20, 2), (2, 2)]
         )
 
     def test3(self):
-        self.assertEqual(solution([100, 100]), [(100, 100.0), (100, 100.0)])
+        self.assertListEqual(solution([1234, 4321]), [(1234, 4321), (4321, 1234)])
 
     def test4(self):
-        self.assertEqual(
-            solution([12, 45, 67, 34, 89, 56]),
-            [
-                (12, 25.92),
-                (45, 63.29),
-                (67, 47.73),
-                (34, 47.73),
-                (89, 63.29),
-                (56, 25.92),
-            ],
-        )
+        self.assertListEqual(solution([1111, 1111]), [(1111, 1111), (1111, 1111)])
 
     def test5(self):
-        self.assertEqual(
-            solution([3, 2, 1, 0, 0, 1, 2, 3]),
+        self.assertListEqual(
+            solution([12345, 54321, 11111, 44444, 22222, 88888]),
             [
-                (3, 3.0),
-                (2, 2.0),
-                (1, 1.0),
-                (0, 0.0),
-                (0, 0.0),
-                (1, 1.0),
-                (2, 2.0),
-                (3, 3.0),
+                (12345, 54321),
+                (54321, 12345),
+                (11111, 11111),
+                (44444, 44444),
+                (22222, 22222),
+                (88888, 88888),
             ],
         )
-
-    def test6(self):
-        self.assertEqual(
-            solution([0, 0, 0, 0, 0]),
-            [(0, 0.0), (0, 0.0), (0, 0.0), (0, 0.0), (0, 0.0)],
-        )
-
-    def test7(self):
-        self.assertEqual(solution([0]), [(0, 0.0)])
-
-    def test8(self):
-        self.assertEqual(solution([1]), [(1, 1.0)])
 
 
 if __name__ == "__main__":
