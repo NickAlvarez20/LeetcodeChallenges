aeacb8eff15ec0da6de2b2e8dd553c7a22e03f6c
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-05-31 16:02:27 -0700

    solve: solve odds and evens product iteration

--- Arrays/cosmo.py
+++ Arrays/cosmo.py
@@ -1,21 +1,17 @@
-import math
+def solution(n):
+    num_odds = 0
+    digit_product = 1
+    while n > 0:
+        digit = n % 10
+        # if odd, increment num_odds and multiply
+        if digit % 2 == 1:
+            num_odds += 1
+            digit_product *= digit
+        elif digit % 2 == 0:
+            pass
+        n = n // 10
+    if num_odds == 0:
+        return 0
+    else:
+        return digit_product
 
-
-def solution(numbers):
-    n = len(numbers)  # len of original arr
-    result = []  # for determining pairs
-    result_two = []  # for sums of pairs
-    slice_len = 0  # determine the slice length
-    # grab the pairs
-    for i in range(n):
-        result.append([numbers[i], numbers[n - i - 1]])
-
-    slice_len = math.ceil(len(result) / 2)  # accounts for odd and even arrs
-    sliced_result = result[
-        0:slice_len:1
-    ]  # slice and store the array of pairs up to a certain point
-    len_new_arr = len(sliced_result)  # len of new sliced result
-    # sum the pairs and output to new result arr
-    for i in range(len_new_arr):
-        result_two.append(sum(sliced_result[i]))
-    return result_two
