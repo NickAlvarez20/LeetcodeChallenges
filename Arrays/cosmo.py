e5906062bbd6e8d8bbd2701266945ae492de3a3b
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-05-31 12:43:55 -0700

    solve: Solve opposite geometric means

--- Arrays/cosmo.py
+++ Arrays/cosmo.py
@@ -1,26 +1,10 @@
-# Space exploration crew members' data, containing their names, missions, and roles
-crew_data = (
-    "Neil,Armstrong,Apollo 11,C;Buzz,Aldrin,Apollo 11,P;Michael,Collins,Apollo 11,CM"
-)
+import math
 
-crew_data = crew_data.split(";")
-clean_data = []
 
-for person in crew_data:
-    first, last, mission, role = person.split(",")
-    clean_data.append((f"{first} {last} {mission} {role}"))
-
-print("\n".join(clean_data))
-
-# TODO: Split the crew_data string into a list of individual crew member information using the appropriate delimiter
-
-# TODO: Iterate over the list of crew member data
-
-# TODO: For each member, split their data string using commas as delimiters
-
-# TODO: Print the crew member's details in a formatted string
-
-# Expected output:
-# Neil Armstrong Apollo 11 C
-# Buzz Aldrin Apollo 11 P
-# Michael Collins Apollo 11 CM
+def solution(numbers):
+    result = []
+    n = len(numbers)
+    for i in range(n):
+        square_root = round(math.sqrt((numbers[i] * numbers[n - i - 1])), 2)
+        result.append((numbers[i], square_root))
+    return result
