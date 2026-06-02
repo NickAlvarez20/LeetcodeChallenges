e7c0232ea0f8fb208aac8bb22ba1d48b4585d8af
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-02 14:24:37 -0700

    resolve(practice): Solidify foundational patterns for string traversal for mid length traversals

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,14 +1,10 @@
 def special_order(inputString):
-    # create result var for updating string 
-    result = ''
-    # create length for easier readability
-    length = len(inputString)
-
-    # iterate through and build string starting from back of loop
+    result = ""  # Create result var for building string
+    length = len(inputString)  # Easier readability
     for i in range(length // 2 + length % 2):
-        result += inputString[length-1-i]
+        result += inputString[length - 1 - i]
     for i in range(length // 2 + length % 2):
-        if length - 1 - i != i: # checks for odd indices, so if i doesn't equal the index like 4 then add it, this prevents duplicates
+        if length - 1 - i != i:
             result += inputString[i]
     return result
 
