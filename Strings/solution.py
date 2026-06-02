46c0ca3fc54b28fcd5ffdf1ed26f37b94c2d6f8a
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-02 14:39:13 -0700

    resolve(practice): Solidify foundational patterns for string traversal for mid length traversals

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,14 +1,13 @@
 def special_order(inputString):
-    res = ""
+    result = ""
     length = len(inputString)
 
     for i in range(length // 2 + length % 2):
-        res += inputString[length - 1 - i]
+        result += inputString[length - 1 - i]
     for i in range(length // 2 + length % 2):
         if length - 1 - i != i:
-            res += inputString[i]
-
-    return res
+            result += inputString[i]
+    return result
 
 
 test = special_order("abcdefghijklmnopqrstuvwxyz" * 4 + "abcd")
