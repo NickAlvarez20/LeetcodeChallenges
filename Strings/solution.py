f19d79c1194e261ec187cfacd8b87b95262b0ca1
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-04 15:34:18 -0700

    practice: Internalize pattern for mid level traversals

--- Strings/solution.py
+++ Strings/solution.py
@@ -2,20 +2,18 @@ def solution(numbers):
     mid = len(numbers) // 2
 
     if len(numbers) % 2 == 1:
-        left = mid-1
+        left = mid - 1
         right = mid + 1
         tuple_list = [(numbers[mid], 0)]
     else:
         left = mid - 1
         right = mid
         tuple_list = []
-    
+
     while left >= 0 and right < len(numbers):
         tuple_list.append((numbers[left], numbers[right]))
         left -= 1
         right += 1
-        print(tuple_list)
-
     return tuple_list
 
 
