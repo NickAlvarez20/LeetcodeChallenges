eb8288057a8eac26c3e0641bd4c8e36705eeee7c
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-04 23:04:58 -0700

    practice: Internalize pattern for mid level traversals

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,6 +1,5 @@
 def unusual_traversal(arr):
     n, mid = len(arr), len(arr) // 2
-
     if n % 2 == 1:
         left = mid - 1
         right = mid + 1
@@ -10,7 +9,7 @@ def unusual_traversal(arr):
         right = mid
         result = []
 
-    while left >= 0:
+    while left >= 0 or right < n:
         if left - 1 >= 0:
             result.append(arr[left - 1])
         if left >= 0:
@@ -19,7 +18,6 @@ def unusual_traversal(arr):
             result.append(arr[right])
         if right + 1 < n:
             result.append(arr[right + 1])
-
         left -= 2
         right += 2
 
