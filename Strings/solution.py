e9e810f64af5427c524681d059ecac649c45ba35
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-05 21:14:20 -0700

    solve(repeat): Repeat and internalize pattern, flawless

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,26 +1,26 @@
 def encode_rle(s):
     groups = []
-    current_group_char = None
-    current_group_length = 0
-    res = ''
+    curr_group_char = None
+    curr_group_len = 0
+    res = ""
 
     for char in s:
         if char.isalnum():
-            if char == current_group_char:
-                current_group_length += 1
+            if char == curr_group_char:
+                curr_group_len += 1
             else:
-                if current_group_char is not None:
-                    groups.append((current_group_char, current_group_length))
-                current_group_char = char
-                current_group_length = 1
-    if current_group_char is not None:
-        groups.append((current_group_char, current_group_length))
+                if curr_group_char is not None:
+                    groups.append((curr_group_char, curr_group_len))
+                curr_group_char = char
+                curr_group_len = 1
+
+    if curr_group_char is not None:
+        groups.append((curr_group_char, curr_group_len))
 
     for char, length in groups:
         res += f"{char}{length}"
 
     return res
-
 
 
 print(encode_rle("aaa@@bb!!c#d**e"))
