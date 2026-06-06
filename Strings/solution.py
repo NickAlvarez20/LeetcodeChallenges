44faae6e0c20edeaaf5ad21928148c1c4d894e27
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-06 01:48:26 -0700

    solve(practice str ops & type convs Python): learn how to rotate using join, slices, concatenation, list comprehension, and split witin list comprehension for clean 1 liner

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,28 +1,11 @@
 def solution(s):
-    group = []
-    curr_chars_in_group = None
-    curr_chars_count = 0
-    res = ''
-
-    for index in range(0, len(s), 2):
-        curr_block = s[index:index+2]
-        if curr_block == curr_chars_in_group:
-            curr_chars_count += 1
-        else:
-            if curr_chars_in_group is not None:
-                group.append((curr_chars_in_group, curr_chars_count))
-            curr_chars_in_group = curr_block
-            curr_chars_count = 1
-    if curr_chars_in_group is not None:
-        group.append((curr_chars_in_group, curr_chars_count))
-
-
-    for char, length in group:
-        res += f"{char}{length}"
-
-    return res
-
+    
+    return " ".join([word[-1]+word[:-1] for word in s.split(" ")])
+    
+ 
+    
+    
+    
     
 
-
-print(solution("aaababbababaca"))
+print(solution("abc 123 def ghi"))
--- Strings/solution_test.py
+++ Strings/solution_test.py
@@ -2,21 +2,32 @@ import unittest
 from solution import solution
 
 
-class SolutionTests(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(solution("aaababbababaca"), "aa1ab2ba3ca1")
+class TestSolution(unittest.TestCase):
+    def test_input_1(self):
+        self.assertEqual(solution("abc 123 def ghi"), "cab 312 fde igh")
 
-    def test2(self):
-        self.assertEqual(solution("abcabcabcabcab"), "ab1ca1bc1ab1ca1bc1ab1")
+    def test_input_2(self):
+        self.assertEqual(solution("bat"), "tba")
 
-    def test3(self):
-        self.assertEqual(solution("ab"), "ab1")
+    def test_input_3(self):
+        self.assertEqual(solution("raceCar"), "rraceCa")
 
-    def test4(self):
-        self.assertEqual(solution("ccddaaeeff"), "cc1dd1aa1ee1ff1")
+    def test_input_4(self):
+        self.assertEqual(solution("mAnGo666 TaCo123i"), "6mAnGo66 iTaCo123")
 
-    def test5(self):
-        self.assertEqual(solution("eeffgg"), "ee1ff1gg1")
+    def test_input_5(self):
+        self.assertEqual(solution("_ab 77Y UwF88"), "b_a Y77 8UwF8")
+
+    def test_input_6(self):
+        self.assertEqual(solution("SingleWord"), "dSingleWor")
+
+    def test_input_7(self):
+        self.assertEqual(solution("abcdefghij"), "jabcdefghi")
+
+    def test_input_8(self):
+        self.assertEqual(
+            solution("ZzZzZzZ 1234567890 zYxWvUtS"), "ZZzZzZz 0123456789 SzYxWvUt"
+        )
 
 
 if __name__ == "__main__":
