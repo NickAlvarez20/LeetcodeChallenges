5837c59b7e8b3fbece53a3ade59888c0e372d460
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-06 01:09:06 -0700

    complete(applying simple looping in practice with python): Complete last exercise and win certificate

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,23 +1,28 @@
 def solution(s):
-    groups = []
-    curr_group_char = None
-    curr_group_length = 0
+    group = []
+    curr_chars_in_group = None
+    curr_chars_count = 0
+    res = ''
 
-    for char in s:
-        if char == curr_group_char:
-            curr_group_length += 1
+    for index in range(0, len(s), 2):
+        curr_block = s[index:index+2]
+        if curr_block == curr_chars_in_group:
+            curr_chars_count += 1
         else:
-            if curr_group_char is not None:
-                groups.append((curr_group_char, curr_group_length))
-            curr_group_char = char
-            curr_group_length = 1
-
-    if curr_group_char is not None:
-        groups.append((curr_group_char, curr_group_length))
-
-    groups.reverse()
-
-    return groups
+            if curr_chars_in_group is not None:
+                group.append((curr_chars_in_group, curr_chars_count))
+            curr_chars_in_group = curr_block
+            curr_chars_count = 1
+    if curr_chars_in_group is not None:
+        group.append((curr_chars_in_group, curr_chars_count))
 
 
-print(solution("aaabbcccdde"))
+    for char, length in group:
+        res += f"{char}{length}"
+
+    return res
+
+    
+
+
+print(solution("aaababbababaca"))
