c4e3535fd88117cff4bc94ce660afbf98e691773
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-10 20:30:47 -0700

    refactor(hash tables): Refactor answer to optimal solution and understand alternative variation

--- HashTables/count_elems_max_freq.py
+++ HashTables/count_elems_max_freq.py
@@ -16,3 +16,14 @@ class Solution:
             if val == max:
                 count += val
         return count
+
+
+# Refactored:
+from collections import Counter
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        frequencies = Counter(nums).values() # Creates an array of max values/frequencies e.g., [2,2,1,1]
+        max_freq = max(frequencies) # Sets max_freq for determining comparison operators
+        return sum(f for f in frequencies if f == max_freq) # sums each individual elem using sum method if match found
