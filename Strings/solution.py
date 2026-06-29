7bd1dbdd58f35e78c66eb3bb868107d4054db400
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-29 14:07:23 -0700

    reinforcement learning: Revisit Prac String Ops & Type Convs in  Python, Unit 4 (Replace Substring in a Text) and rewrite solution from scratch after pondering and understanding

--- Strings/solution.py
+++ Strings/solution.py
@@ -1,20 +1,20 @@
 def replace_substring(text, old, new):
     result_pieces = []
     current_index = 0
-    len_old = len(old)
-
     start_pos = text.find(old)
 
+    len_old = len(old)
+
     while start_pos != -1:
-        result_pieces.append(text[current_index:start_pos])
+        result_pieces.append(text[current_index:start_pos]) # append the current up to start_pos index
 
-        result_pieces.append(new)
+        result_pieces.append(new) # append new word to replace old word
 
-        current_index = start_pos + len_old
+        current_index = start_pos + len_old # moves index to after the current word
 
-        start_pos = text.find(old, current_index)
-    result_pieces.append(text[current_index:])
+        start_pos = text.find(old, current_index) # finds using old as keyword and starts searching from current index updated pos
 
+    result_pieces.append(text[current_index:]) # appends everything that is after the last old word pos that is found
     return "".join(result_pieces)
 
 
