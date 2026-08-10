858f5cb0f2e3fb85ce93a10e170bc73f93176bf1
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-10 11:13:16 -0700

    complete ISDSP(Unit 1): Complete all lessons within Unit 1

---
+++ CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_1/Adjust Your Travel Itinerary with Python/solution.py
@@ -0,0 +1,8 @@
+# Our list of destinations
+travel_destinations = ["Paris", "Sydney", "Tokyo", "New York", "London"]
+
+# Todo: Insert "Berlin" at the third position using the insert() function
+travel_destinations.insert(2, "Berlin")
+
+# Checking the list
+print(travel_destinations)
---
+++ CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_1/Creating and Accessing Nordic Travel List/solution.py
@@ -0,0 +1,11 @@
+# TODO: Establish a list to store travel destinations
+# Hint: Use square brackets to denote the list: list_name = [elements, ...]
+travel_destinations = ["Oslo", "Copenhagen", "Reykjavik", "Helsinki", "Stockholm"]
+
+# TODO: Find the first destination on the list
+# Hint: You can access the first element in a list using list_name[0]
+print(travel_destinations[0])
+
+# TODO: Find the last destination on the list
+# Hint: Python allows you to find the last item in a list using list_name[-1]
+print(travel_destinations[-1])
