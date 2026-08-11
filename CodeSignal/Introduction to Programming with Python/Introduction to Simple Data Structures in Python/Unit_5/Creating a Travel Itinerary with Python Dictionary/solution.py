9baad3565613bc07a0fdaea53596d3309ef820dd
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-10 23:00:45 -0700

    IPP/ISDSP(Unit 5): Complete Unit 5 learnings with nested dictionaries and methods

--- CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_5/Creating a Travel Itinerary with Python Dictionary/solution.py
+++ CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_5/Creating a Travel Itinerary with Python Dictionary/solution.py
@@ -1,1 +1,7 @@
-# Placeholder for Lesson 5
+# TODO: Construct a nested dictionary
+itinerary = {
+    "Stop1": {"city": "Rome", "country": "Spain", "airport_code": "FCO"},
+    "Stop2": {"city": "New York", "country": "USA", "airport_code": "JFK"},
+    "Stop3": {"city": "Rio de Janeiro", "country": "Brazil", "airport_code": "SDU"},
+}
+print(itinerary)
--- CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_5/Running and Inspecting a Nested Python Dictionary/solution.py
+++ CodeSignal/Introduction to Programming with Python/Introduction to Simple Data Structures in Python/Unit_5/Running and Inspecting a Nested Python Dictionary/solution.py
@@ -1,1 +1,9 @@
-# Placeholder for Lesson 1
+# A dictionary to handle airport codes
+airport_codes = {
+    "JFK": {"city": "New York", "country": "USA"},
+    "LAX": {"city": "Los Angeles", "country": "USA"},
+    "LHR": {"city": "London", "country": "UK"},
+}
+
+# Output the contents of the dictionary
+print(airport_codes)
