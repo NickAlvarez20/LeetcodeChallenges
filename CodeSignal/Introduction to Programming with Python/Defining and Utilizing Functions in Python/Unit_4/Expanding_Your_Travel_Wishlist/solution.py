3356edfd5222065c6e792a37f6fb097e95ec5748
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-11 05:07:55 -0700

    complete IPP/DUFP(Unit 4): Complete and understand variable scope within function and how to add global scope variable to dynamically update within function calls

--- CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_4/Expanding_Your_Travel_Wishlist/solution.py
+++ CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_4/Expanding_Your_Travel_Wishlist/solution.py
@@ -1,1 +1,13 @@
-#placeholder
+# Global list of countries in the travel wishlist
+travel_wishlist = ["Greece", "Japan"]
+
+
+def add_to_wishlist(country):
+    # TODO: Add the country to the travel_wishlist using the append method
+    travel_wishlist.append(country)
+
+
+add_to_wishlist("Canada")
+print(
+    travel_wishlist
+)  # This should print ["Greece", "Japan", "Canada"] after you add the missing code.
--- CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_4/Global_Landmarks_Tracker_Challenge/solution.py
+++ CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_4/Global_Landmarks_Tracker_Challenge/solution.py
@@ -1,1 +1,20 @@
-#placeholder
+# TODO: Declare a global list to keep track of visited landmarks
+visited_landmarks = [
+    "Colosseum in Rome",
+    "Pyramids of Giza in Egypt",
+    "Christ the Redeemer in Rio de Janeiro",
+]
+
+
+# TODO: Define a function named log_landmark that takes two parameters: landmark and city
+def log_landmark(landmark, city):
+    visited_landmarks.append(f"{landmark} in {city}")
+
+    # TODO: Add the landmark and its city to the global list in the format "landmark in city"
+
+
+# TODO: Call the log_landmark function with examples e.g., "Eiffel Tower" and "Paris"
+log_landmark("Eiffel Tower", "Paris")
+
+# TODO: Print the list of visited landmarks
+print(visited_landmarks)
