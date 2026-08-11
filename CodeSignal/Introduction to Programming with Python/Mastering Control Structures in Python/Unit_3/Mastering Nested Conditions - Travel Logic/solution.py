a1e5684a58b646717f2bfc3585f5d45ceacf92b1
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-10 23:42:39 -0700

    complete IPP/MCSP(Unit 3): Complete unit 3 understandings and practice lessons for nested conditionals

---
+++ CodeSignal/Introduction to Programming with Python/Mastering Control Structures in Python/Unit_3/Mastering Nested Conditions - Travel Logic/solution.py
@@ -0,0 +1,18 @@
+# TODO: Declare variables 'age' and 'budget' with appropriate values
+
+age = 25
+budget = 1200
+# TODO: Write an 'if' statement to determine if the traveler is over 18
+if age > 18:
+    # TODO: Inside the 'if' block, write another 'if' statement to check if the budget is more than 1000
+    if budget > 1000:
+        # TODO: Print "You are eligible for the international travel package." for budgets over 1000
+        print("You are eligible for the international travel package.")
+    # TODO: Else block for budgets 1000 or less
+    else:
+        print("You are eligible for the local travel package.")
+        # TODO: Print "You are eligible for the local travel package."
+# TODO: Else block for travelers 18 or below
+else:
+    # TODO: Print "You are eligible for the children's travel package."
+    print("You are eligible for the children's travel package.")
