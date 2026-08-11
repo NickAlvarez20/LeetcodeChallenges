6f2dea86fc3e85b369725b4f368760874b606df9
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-11 04:47:15 -0700

    complete IPP/DUFP(Unit 2): Complete unit 2 lessons and exercises, learning about functions with parameters and invoking calls.

---
+++ CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_2/Personalizing_Greetings_with_City_Mention/solution.py
@@ -0,0 +1,9 @@
+# TODO: Write a function named greet_with_welcome that takes two parameters: name and hometown.
+# The function should print a greeting to the user that includes their name and hometown.
+
+
+def greet_with_welcome(name, hometown):
+    print(f"Hello, {name}! Welcome back to {hometown}!")
+
+
+greet_with_welcome("Morgan", "Program City")
---
+++ CodeSignal/Introduction to Programming with Python/Defining and Utilizing Functions in Python/Unit_2/Running_Functions_with_Parameters/solution.py
@@ -0,0 +1,7 @@
+# Define a function to greet the user by name
+def greet_user_by_name(name):
+    print(f"Hello, {name}!")
+
+
+# Call the function
+greet_user_by_name("Alex")
