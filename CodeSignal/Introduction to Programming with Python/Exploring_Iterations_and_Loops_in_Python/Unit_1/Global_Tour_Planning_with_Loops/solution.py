404fa7ad4cb013c9a71312d734b7edd764fe7630
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-11 00:43:58 -0700

    complete IPP/EILP(Unit 1): Complete unit 1 for loop practice and lessons

---
+++ CodeSignal/Introduction to Programming with Python/Exploring_Iterations_and_Loops_in_Python/Unit_1/Global_Tour_Planning_with_Loops/solution.py
@@ -0,0 +1,8 @@
+# TODO: Create a list named tour_countries with the countries "France", "Italy", "Spain", "Japan"
+tour_countries = ["France", "Italy", "Spain", "Japan"]
+
+# TODO: Use a for loop to iterate over tour_countries
+for country in tour_countries:
+    print(f"Considering {country} for the global tour.")
+
+    # TODO: Print each country in the format: "Considering [Country] for the global tour."
