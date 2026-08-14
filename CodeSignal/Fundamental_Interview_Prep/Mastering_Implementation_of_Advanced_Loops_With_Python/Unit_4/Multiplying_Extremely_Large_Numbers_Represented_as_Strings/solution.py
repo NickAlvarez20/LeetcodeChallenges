e3a722c5f65f1979280a79a8d7676b0149e00198
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-08-14 02:34:00 -0700

    complete FIP/MIALP(Unit 4): Complete Unit 4 exercises and learnings with fundamental string to int to string addition, subtract, comparison, and multipying large numbers.

--- CodeSignal/Fundamental_Interview_Prep/Mastering_Implementation_of_Advanced_Loops_With_Python/Unit_4/Multiplying_Extremely_Large_Numbers_Represented_as_Strings/solution.py
+++ CodeSignal/Fundamental_Interview_Prep/Mastering_Implementation_of_Advanced_Loops_With_Python/Unit_4/Multiplying_Extremely_Large_Numbers_Represented_as_Strings/solution.py
@@ -1,1 +1,23 @@
-#placeholder
+def solution(num1, num2):
+    result = [0] * (len(num1) + len(num2))
+    carry = 0
+
+    for i in range(len(num2) - 1, -1, -1):
+        for j in range(len(num1) - 1, -1, -1):
+            product = int(num2[i]) * int(num1[j])
+            result[
+                i + j + 1
+            ] += product  # add to the end of result, updating the very right position
+            result[i + j] += (
+                result[i + j + 1] // 10
+            )  # updates right position with ones positon
+            result[i + j + 1] %= 10  # updates left position with tens position
+
+    stringified = [str(digit) for digit in result]
+
+    final_value = "".join(stringified).lstrip("0")
+
+    if final_value == "":
+        return "0"
+    else:
+        return final_value
