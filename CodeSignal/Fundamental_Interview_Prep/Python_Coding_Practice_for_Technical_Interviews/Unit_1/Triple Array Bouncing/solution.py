b405798420732bd98a71ceb0dbeafff63b7d57f8
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-09-03 16:17:37 -0700

    solve (FCIPP/PCPTI-Unit 1/Q3) - Solve Triple Array Bouncing Adventure with Gloria and learn how to set up incrementally counter, edge case checks for out of bounds conditions, tuple checks within a set, adding a tuple to set, updating max values, and finally returning the maxArrayB + maxArrayC values to solve

--- CodeSignal/Fundamental_Interview_Prep/Python_Coding_Practice_for_Technical_Interviews/Unit_1/Triple Array Bouncing/solution.py
+++ CodeSignal/Fundamental_Interview_Prep/Python_Coding_Practice_for_Technical_Interviews/Unit_1/Triple Array Bouncing/solution.py
@@ -1,1 +1,55 @@
-# placeholder
+def solution(arrayA, arrayB, arrayC):
+    max_valueB = 0
+    max_valueC = 0
+    indexA = 0
+    indexB = None
+    indexC = None
+    visited = set()
+    curr_step = 0
+
+    while True:
+        if curr_step == 0:  # hops to B
+            indexB = arrayA[indexA]  # grab next hop
+            if indexB >= len(arrayB):
+                break
+            if (indexB, "A") in visited:
+                break
+            else:
+                visited.add((indexB, "A"))
+
+            if arrayB[indexB] > max_valueB:
+                max_valueB = arrayB[indexB]
+
+            curr_step += 1
+        elif curr_step == 1:  # hops to A
+            indexA = arrayB[indexB]
+            if indexA >= len(arrayA):
+                break
+            if (indexA, "B") in visited:
+                break
+            else:
+                visited.add((indexA, "B"))
+
+            curr_step += 1
+        elif curr_step == 2:  # hops to C
+            indexC = arrayA[indexA]
+            if indexC >= len(arrayC):
+                break
+            if (indexC, "C") in visited:
+                break
+            if arrayC[indexC] > max_valueC:
+                max_valueC = arrayC[indexC]
+            visited.add((indexC, "A"))
+
+            curr_step += 1
+        elif curr_step == 3:  # hops to A
+            indexA = arrayC[indexC]
+            if indexA >= len(arrayA):
+                break
+            if (indexA, "C") in visited:
+                break
+            else:
+                visited.add((indexA, "C"))
+            curr_step = 0
+
+    return max_valueB + max_valueC
